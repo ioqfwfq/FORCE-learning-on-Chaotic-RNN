d8b6cabbc61da9e30ccda658fd2f089e93cd3419
JZhu <jundachriszhu@gmail.com>
2021-04-13 17:49:24 -0400

    Update and rename RNN_v06_4.m to RNN_v06_5.m
    Update: from v06.4, now with any pairs of pulse input function to control any output

--- RNN_v06_5.m
+++ RNN_v06_5.m
@@ -1,21 +1,22 @@
-function RNN_v06_4(varargin)
-% RNN_v06.4 A recurrent neural network with certain training phase
+function RNN_v06_5(varargin)
+% RNN_v06.5 A recurrent neural network with certain training phase
 % Ref: Susillo and Abbott, 2009
 % This version sets up the basic flow of the program, with FORCE training
+% only on Wout!
 % It plots the activity of nGN and actual output z.
-% Update: from v06.3, now with two pairs of pulse input function to control two output
+% Update: from v06.4, now with any pairs of pulse input function to control any output
 
 % v01 by Emilio Salinas, January 2021
-% Junda Zhu, 4-6-2021
+% Junda Zhu, 4-12-2021
 % clear all
 clf
 %% parameters
 para = varargin{1};
 if length(para) ~= 8
     % network parameters
-    nGN = 200;     % number of generator (recurrent) neurons
+    nGN = 1200;     % number of generator (recurrent) neurons
     tau = 10;    % membrane time constant, in ms
-    p_GG = 1; % p of non zero recurrence
+    p_GG = 0.8; % p of non zero recurrence
     p_z = 1; % p of non zero output
     alpha = 1;
     g = 1.5;
@@ -33,12 +34,12 @@ else % parameters given by user input
     Ttrain = para(7);
     dt = para(8);
 end
-nplot = 4;
+nplot = 8;
 if nplot > nGN
     nplot = nGN;
 end
 
-numoutput = 2;% number of output
+numoutput = 8;% number of output
 %% initialize arrays
 x = gpuArray(2*rand(nGN,1,'single') - 1);
 J = gpuArray(zeros(nGN,'single'));
@@ -49,7 +50,7 @@ JGi = gpuArray(zeros(nGN,2*numoutput,'single'));
 JGi(randperm(length(JGi(:)),p_GG*length(JGi(:)))) = randn(p_GG*length(JGi(:)),1);
 W = gpuArray(randn(nGN,numoutput,'single')/sqrt(p_z*nGN)); %output weight vector
 P = gpuArray(eye(nGN,'single')/alpha); %update matrix
-z = gpuArray(zeros(numoutput,1,'single')); %output
+z = gpuArray(zeros(numoutput,1,'single')) -1; %output
 eneg = gpuArray(zeros(numoutput,1,'single'));
 
 % set space for data to be plotted
@@ -75,7 +76,7 @@ for i=1:T_start
     H = tanh(x); % firing rates
     z = W' * H; % output
     dw = - P * H * eneg'; %dw
-    dxdt = (-x + J*H) / tau;
+    dxdt = (-x + J*H + JGz*z) / tau;
     x = x + dxdt*dt;
     t = t + dt;
     
@@ -90,33 +91,29 @@ con = 1;
 while con
     disp('Training Start');
     tic
-    % precompute target and input function
+    % target and input function
     wid = 20; % 20 ms
-    d = rand(40,1)*length(T_start:T_end);
-    pul = rectpuls(1:length(T_start:T_end),wid);
-    I(1,T_start:T_end) = pulstran(1:length(T_start:T_end),d(1:10),pul)';
-    I(2,T_start:T_end) = pulstran(1:length(T_start:T_end),d(11:20),pul)';
-    I(3,T_start:T_end) = pulstran(1:length(T_start:T_end),d(21:30),pul)';
-    I(4,T_start:T_end) = pulstran(1:length(T_start:T_end),d(31:40),pul)';
-    f = [f zeros(numoutput,length(T_start:T_end))] -1;
     
+    for ij = 1:numoutput
+        d = rand(length(T_start:T_end)/500,1)*length(T_start:T_end); % random time points for the inputs
+        pul = rectpuls(1:length(T_start:T_end),wid); % pulses
+        I(2*ij-1,T_start:T_end) = pulstran(1:length(T_start:T_end),d(1:length(d)/2-1),pul)'; % On
+        I(2*ij,T_start:T_end) = pulstran(1:length(T_start:T_end),d(length(d):-1:length(d)/2),pul)'; % Off
+        
+    end
+    f = [f zeros(numoutput,length(T_start:T_end))-1]; % target function at -1
     % Main loop
     dwplot(T_start) = 0;
     for i=T_start:T_end
         f(:,i) = f(:,i-1);
-        if I(1,i)
-            f(1,i)=1;
-            disp(['Training... ' num2str(i) ' / ' num2str(T_end)]);
-        end
-        if I(2,i)
-            f(1,i)=-1;
-        end
-        if I(3,i)
-            f(2,i)=1;
-            disp(['Training... ' num2str(i) ' / ' num2str(T_end)]);
-        end
-        if I(4,i)
-            f(2,i)=-1;
+        for ij = 1:numoutput
+            if I(2*ij-1,i)
+                f(ij,i) = 1;
+                disp(['Training... ' num2str(i) 'ms / ' num2str(T_end) 'ms']);
+            end
+            if I(2*ij,i)
+                f(ij,i) = -1;
+            end
         end
         
         H = tanh(x); % firing rates
@@ -125,10 +122,10 @@ while con
         eneg = z - f(:,i); % error
         dw = - P * H * eneg';
         W = W + dw; % update W
-        J = J + repmat(dw(:,1)', nGN, 1) + repmat(dw(:,2)', nGN, 1); %update J (recurrent)
+        %         J = J + repmat(dw(:,1)', nGN, 1) + repmat(dw(:,2)', nGN, 1); %update J (recurrent)
         z = W' * H; % output
         epos = z - f(:,i); % error after update
-        dxdt = (-x + J*H + JGi*I(:,i)) / tau;
+        dxdt = (-x + J*H + JGz*z + JGi*I(:,i)) / tau;
         x = x + dxdt*dt;
         t = t + dt;
         
@@ -143,37 +140,32 @@ while con
     toc
     % testing
     wid = 20; % 20 ms
-    d = rand(12,1)*length(T_end+1:T_end+Ttrain);
-    pul = rectpuls(1:length(T_end+1:T_end+Ttrain),wid);
-    I(1,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(1:3),pul)';
-    I(2,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(4:6),pul)';
-    I(3,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(7:9),pul)';
-    I(4,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(10:12),pul)';
-    f = [f zeros(numoutput,length(T_end+1:T_end+Ttrain))] -1;
-    
+    for ij = 1:numoutput
+        d = rand(length(T_end+1:T_end+Ttrain)/500,1)*length(T_end+1:T_end+Ttrain); % random time points for the inputs
+        pul = rectpuls(1:length(T_end+1:T_end+Ttrain),wid); % pulses
+        I(2*ij-1,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(1:length(d)/2-1),pul)'; % On
+        I(2*ij,T_end+1:T_end+Ttrain) = pulstran(1:length(T_end+1:T_end+Ttrain),d(length(d):-1:length(d)/2),pul)'; % Off
+    end
+    f = [f zeros(numoutput,length(T_end+1:T_end+Ttrain))-1]; % target function at -1
     dwplot(T_end+1:T_end+Ttrain) = 0;
+    
     for i = T_end+1:T_end+Ttrain
         f(:,i) = f(:,i-1);
-        if I(1,i)
-            f(1,i)=1;
-            disp(['Testing... ' num2str(i) 'ms / ' num2str(T_end+Ttrain) 'ms']);
-        end
-        if I(2,i)
-            f(1,i)=-1;
-        end
-        if I(3,i)
-            f(2,i)=1;
-            disp(['Testing... ' num2str(i) 'ms / ' num2str(T_end+Ttrain) 'ms']);
-        end
-        if I(4,i)
-            f(2,i)=-1;
+        for ij = 1:numoutput
+            if I(2*ij-1,i)
+                f(ij,i) = 1;
+                disp(['Testing... ' num2str(i) 'ms / ' num2str(T_end+Ttrain) 'ms']);
+            end
+            if I(2*ij,i)
+                f(ij,i) = -1;
+            end
         end
         
         H = tanh(x); % firing rates
         eneg = z - f(:,i);
         z = W' * H; % output
         epos = z - f(:,i);
-        dxdt = (-x + J*H + JGi*I(:,i)) / tau;
+        dxdt = (-x + J*H + JGz*z + JGi*I(:,i)) / tau;
         x = x + dxdt*dt;
         t = t + dt;
         
@@ -192,36 +184,25 @@ while con
     clrOut = 'r';
     clrF = 'g';
     clr_grid = 0.5*[1 1 1];
-    % scale factor for plotting activity one neuron per row
-    sfac = 0.5;
-    subplot(4,1,1)
-    hold on
-    %     xlim([0 T_end+Ttrain])
-    %     ylim([-.2 1.2])
-    %     set(gca, 'YTick', [-1, 0, 1])
-    patch([T_start T_start T_end T_end],[-1.2, 6.2, 6.2, -1.2],'r', 'FaceAlpha',0.1,'EdgeAlpha',0.1);
-    plot(tplot(T_start:T_end+Ttrain), f(1,T_start:T_end+Ttrain)+5, '-', 'color', clrF, 'LineWidth', 2);
-    plot(tplot(T_start:T_end+Ttrain), zplot(1,T_start:T_end+Ttrain)+5, '-', 'color', clrOut, 'LineWidth', 2);
-    plot(tplot(T_start:T_end+Ttrain), I(1,T_start:T_end+Ttrain)+2, '-r', 'LineWidth', 1);
-    plot(tplot(T_start:T_end+Ttrain), I(2,T_start:T_end+Ttrain), '-b', 'LineWidth', 1);
-    ylabel('Output 1');
-    xlabel('Time (ms)');
+    sfac = 0.5;% scale factor for plotting activity one neuron per row
+    
+    f1 = figure('Name', 'Results');
     title(['RNN v06: ' num2str(nGN) ' neurons, with input']);
+    for ij = 1:numoutput
+        subplot(2+numoutput,1,ij)
+        ylim([-1.2, 7.2])
+        hold on
+        patch([T_start T_start T_end T_end],[-1.2, 7.2, 7.2, -1.2],'r', 'FaceAlpha',0.1,'EdgeAlpha',0.1);
+        plot(tplot(T_start:T_end+Ttrain), I(2*ij-1,T_start:T_end+Ttrain)+2, '-m', 'LineWidth', 1);
+        plot(tplot(T_start:T_end+Ttrain), I(2*ij,T_start:T_end+Ttrain), '-b', 'LineWidth', 1);
+        plot(tplot(T_start:T_end+Ttrain), f(ij,T_start:T_end+Ttrain)+5, '-', 'color', clrF, 'LineWidth', 2);
+        plot(tplot(T_start:T_end+Ttrain), zplot(ij,T_start:T_end+Ttrain)+5, '-', 'color', clrOut, 'LineWidth', 2);
+        
+        ylabel(['Output ' num2str(ij)]);
+        xlabel('Time (ms)');
+    end
     
-    subplot(4,1,2)
-    hold on
-    %     xlim([0 T_end+Ttrain])
-    %     ylim([-1.2 1.2])
-    %         set(gca, 'YTick', [0])
-    patch([T_start T_start T_end T_end],[-1.2, 6.2, 6.2, -1.2],'r', 'FaceAlpha',0.1,'EdgeAlpha',0.1);
-    plot(tplot(T_start:T_end+Ttrain), f(2,T_start:T_end+Ttrain)+5, '-', 'color', clrF, 'LineWidth', 2);
-    plot(tplot(T_start:T_end+Ttrain), zplot(2,T_start:T_end+Ttrain)+5, '-', 'color', clrOut, 'LineWidth', 2);
-    plot(tplot(T_start:T_end+Ttrain), I(3,T_start:T_end+Ttrain)+2, '-r', 'LineWidth', 1);
-    plot(tplot(T_start:T_end+Ttrain), I(4,T_start:T_end+Ttrain), '-b', 'LineWidth', 1);
-    ylabel('Output 2');
-    xlabel('Time (ms)');
-    
-    subplot(4,1,3)
+    subplot(2+numoutput,1,[2+numoutput-1 2+numoutput])
     hold on
     %     xlim([0 T_end+Ttrain])
     ylim([0.25 nplot+0.75])
@@ -235,7 +216,8 @@ while con
     ylabel('Recurrent neurons');
     xlabel('Time (ms)');
     
-    subplot(4,1,4)
+    f2 = figure('Name', 'Performance');
+    subplot(2+numoutput,1,2+numoutput)
     hold on
     %     xlim([0 T_end+Ttrain])
     ylim([-0.15 0.15])
@@ -243,8 +225,8 @@ while con
     line([T_start T_start],[-1 1])
     line([T_end T_end],[-1 1])
     patch([T_start T_start T_end T_end],[-1, 1, 1, -1],'r', 'FaceAlpha',0.1,'EdgeAlpha',0.1)
-    plot(tplot(T_start:T_end+Ttrain), eplot(T_start:T_end+Ttrain), '.', 'color', 'k');
-    plot(tplot(T_start:T_end+Ttrain), dwplot(T_start:T_end+Ttrain)/max(dwplot)*0.3-0.15, '.', 'color', 'c');
+    plot(tplot(T_start:T_end+Ttrain), eplot(T_start:T_end+Ttrain), '.','MarkerSize',2, 'color', 'k');
+    plot(tplot(T_start:T_end+Ttrain), dwplot(T_start:T_end+Ttrain)/max(dwplot)*0.3-0.15, '.','MarkerSize',3, 'color', 'c');
     ylabel('\delta Error');
     xlabel('Time (ms)');
     
